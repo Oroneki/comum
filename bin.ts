5c75c95f2736c79fc1e063c018e7548bf9d1d722
Paulo Fortes <pauloffn@gmail.com>
2018-05-06 11:50:33 -0300

    bin

--- bin.ts
+++ bin.ts
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
-// import {test} from './lib/comum';
 import {comum} from "./lib/comum";
 import {log} from "./lib/util/utils";
-
-// test();
 
 if (!process.argv[2] || !process.argv[3]) {
     log("provide source folder and destination folder.");
